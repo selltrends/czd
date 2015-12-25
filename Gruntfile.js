cfd151dc2bf24e0d7d70f079607877f454019a7f
Alex Wang <selltrends@gmail.com>
2015-12-25 16:39:28 +0800

    amend grunt task for font-awesome correctly displayin grunt:dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -391,6 +391,16 @@ module.exports = function (grunt) {
           cwd: '.',
           src: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*',
           dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          cwd: '.',
+          src: 'bower_components/font-awesome/css/font-awesome.min.css',
+          dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          cwd: '.',
+          src: 'bower_components/font-awesome/fonts/*',
+          dest: '<%= yeoman.dist %>'
         }]
       },
       styles: {
